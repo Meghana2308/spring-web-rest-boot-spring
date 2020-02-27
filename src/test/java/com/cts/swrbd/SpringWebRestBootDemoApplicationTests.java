7994efa0b92d448c459d84bb43b9a0bfb3aaec2d
Meghana <meghanadamerla2308@gmail.com>
2020-02-27 02:07:49 -0800

    Spring-web-rest-boot-testing

---
+++ src/test/java/com/cts/swrbd/SpringWebRestBootDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cts.swrbd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebRestBootDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	
+
+}
